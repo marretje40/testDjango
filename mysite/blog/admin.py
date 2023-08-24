8fe4e9aa658a410ef9f9620b14fc51321a7d5cb1
marinaw <marretje40@hotmail.com>
2023-08-24 17:37:32 +0200

    voetbalspelers weg

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post
-from .models import Voetbalspeler 
 
 admin.site.register(Post)
-admin.site.register(Voetbalspeler)
 
 # Register your models here.
