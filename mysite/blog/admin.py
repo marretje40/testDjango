b155df7ccab265ec22d1fc9fddfdee2660db3210
marinaw <marretje40@hotmail.com>
2023-08-24 18:10:57 +0200

    poging zoveel met voetbalspelers er weer bij

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Post
+from .models import Voetbalspeler
 
 admin.site.register(Post)
+admin.site.register(Voetbalspeler)
 
 # Register your models here.
