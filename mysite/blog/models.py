be5fc43291e1d8803a1c0748a6905344d9fa6ec6
marinaw <marretje40@hotmail.com>
2023-08-24 16:48:00 +0200

    blog voetbalspelers erbij

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,3 +15,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Voetbalspeler(models.Model):
+    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    naam_voetballer = models.CharField(max_length=100)
+    actuele_voetbalclub = models.CharField(max_length=100)
+    created_date = models.DateTimeField(default=timezone.now)
+    laatste_wijziging_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
